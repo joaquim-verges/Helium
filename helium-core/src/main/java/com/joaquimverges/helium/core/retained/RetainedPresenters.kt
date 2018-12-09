7ea19f6f896e37bde385fe0a60b43acc817a6f3a
Bryan Nguyen <ngubryan@justin.tv>
2018-12-08 17:05:40 -0800

    update comment, formatting and order

--- helium-core/src/main/java/com/joaquimverges/helium/core/retained/RetainedPresenters.kt
+++ helium-core/src/main/java/com/joaquimverges/helium/core/retained/RetainedPresenters.kt
@@ -9,15 +9,22 @@ import android.support.v4.app.FragmentActivity
  * Convenient way to access retained Presenters.
  * Presenters instantiated this way will be persisted across configuration changes,
  * and only destroys when the activity is really destroyed.
- *
- * @param activity the Activity to bind this Presenter to
- * @param clazz the class of the Presenter to instantiate.
  */
-inline fun <reified P : ViewModel> FragmentActivity.getRetainedPresenter(): P = ViewModelProviders.of(this).get(P::class.java)
+inline fun <reified P : ViewModel> FragmentActivity.getRetainedPresenter(): P {
+    return ViewModelProviders.of(this).get(P::class.java)
+}
+
+inline fun <reified P : ViewModel> Fragment.getRetainedPresenter(): P {
+    return ViewModelProviders.of(this).get(P::class.java)
+}
 
 /**
- * @param factory optional lambda to construct the Presenter yourself if it has non-empty constructors.
+ * @param factory lambda to construct the Presenter yourself if it has non-empty constructors.
  */
-inline fun <reified P : ViewModel> FragmentActivity.getRetainedPresenter(noinline factory: (Class<P>) -> P): P = ViewModelProviders.of(this, PresenterFactory(P::class.java, factory)).get(P::class.java)
+inline fun <reified P : ViewModel> FragmentActivity.getRetainedPresenter(noinline factory: (Class<P>) -> P): P {
+    return ViewModelProviders.of(this, PresenterFactory(P::class.java, factory)).get(P::class.java)
+}
 
-inline fun <reified P : ViewModel> Fragment.getRetainedPresenter(): P = ViewModelProviders.of(this).get(P::class.java)
+inline fun <reified P : ViewModel> Fragment.getRetainedPresenter(noinline factory: (Class<P>) -> P): P {
+    return ViewModelProviders.of(this, PresenterFactory(P::class.java, factory)).get(P::class.java)
+}

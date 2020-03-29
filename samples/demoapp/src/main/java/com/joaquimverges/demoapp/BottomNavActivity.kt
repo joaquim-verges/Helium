a88db7d2aab41ffb133da75582f892c12159d6f8
Joaquim Verges <joaquim.verges@gmail.com>
2020-03-28 18:57:23 -0700

    Prepare for 0.5.0 release

--- samples/demoapp/src/main/java/com/joaquimverges/demoapp/BottomNavActivity.kt
+++ samples/demoapp/src/main/java/com/joaquimverges/demoapp/BottomNavActivity.kt
@@ -1,14 +1,20 @@
 package com.joaquimverges.demoapp
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.joaquimverges.helium.navigation.bottomnav.BottomNavUi
 
 class BottomNavActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val ui = BottomNavUi(layoutInflater, R.menu.my_menu, R.navigation.my_graph)
+        val ui = BottomNavUi(layoutInflater, R.menu.my_menu, R.navigation.my_graph, bottomBarCustomization = {
+            val colors = ContextCompat.getColorStateList(this, R.color.bottom_tab_colors)
+            it.itemIconTintList = colors
+            it.itemTextColor = colors
+        })
         setContentView(ui.view)
     }
 

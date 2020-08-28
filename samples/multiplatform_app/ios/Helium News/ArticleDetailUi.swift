60488f7e661a779ea3d35e95aa8f8d15640e0a66
joaquim-verges <joaquim.verges@gmail.com>
2020-08-28 10:21:32 -0700

    Update ios Detail view

--- samples/multiplatform_app/ios/Helium News/ArticleDetailUi.swift
+++ samples/multiplatform_app/ios/Helium News/ArticleDetailUi.swift
@@ -9,14 +9,47 @@
 import SwiftUI
 import Foundation
 import NewsCommon
+import SDWebImage
+import SDWebImageSwiftUI
 
 struct ArticleDetailUi: View {
     var state: ArticleDetailLogic.DetailState?
     var eventDispatcher: EventDispatcher<ArticleDetailLogic.DetailEvent>
     
     var body: some View {
-        Text(self.state?.article.title ?? "").onTapGesture {
-            self.eventDispatcher.pushEvent(event: ArticleDetailLogic.DetailEventArticleClosed())
+        VStack {
+            HeroImage(url: self.state?.article.urlToImage).onTapGesture {
+                self.eventDispatcher.pushEvent(event: ArticleDetailLogic.DetailEventArticleClosed())
+            }
+            VStack(alignment: HorizontalAlignment.leading, spacing: 20.0) {
+                Text(self.state?.article.title ?? "")
+                Text(self.state?.article.description ?? "")
+            }.padding(20.0)
+        }.frame(maxHeight: .infinity, alignment: .top)
+    }
+    
+    struct HeroImage: View {
+        var url: String?
+        
+        var body: some View {
+            loadImage(url: url)
+        }
+        
+        func loadImage(url: String?) -> some View {
+            if let wrapper = URL(string: url ?? "") {
+                return AnyView(
+                        WebImage(url: wrapper)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxHeight: 300)
+                        
+                )
+            } else {
+                return AnyView(Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                    .foregroundColor(Color.gray)
+                )
+            }
         }
     }
 }
